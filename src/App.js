ba4c2b5d79f609747c2c13561ffc78c024f5025e
Rachel <rstark.smith@gmail.com>
2022-09-27 16:40:23 -0500

    Add React router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
+import { Route, Routes } from "react-router-dom"
+import NavBar from "./components/Navbar"
+import Home from "./components/Home"
+import CollectorList from "./components/CollectorsList"
+import CollectorPage from "./components/CollectorPage"
+import VinylGrade from "./components/VinylGrade"
 
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
-    </div>
-  );
+    <>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/collectors" element={<CollectorList />} />
+        <Route path="/collectors/:id" element={<CollectorPage />} />
+        <Route exact path="/vinylgrade " element={<VinylGrade />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,21 @@
-
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="navbar" >
+            <img 
+                src='https://i.imgur.com/tLdkxvn.png?1' 
+                alt='lego list logo' 
+                className="logo"
+            />
+                <ul className="nav-links">
+                    <div className="menu">
+                        <NavLink exact to="/">Home</NavLink>
+                        <NavLink exact to="/collectors">Collections</NavLink>
+                        <NavLink to="/resources">Vinyl Grading</NavLink>
+                    </div>
+                </ul>
+        </nav>
     )
 }
 
