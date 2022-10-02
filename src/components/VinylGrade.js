e33dae30677d661fe163fc6153d5cf815c0abb02
Rachel <rstark.smith@gmail.com>
2022-10-02 11:18:49 -0500

    Add grade infomration

--- src/components/VinylGrade.js
+++ src/components/VinylGrade.js
@@ -2,8 +2,26 @@
 
 const VinylGrade = () => {
     return (
-        <div>VinylGrade</div>
+        <div>
+            <h2>How to determine the condition of your record</h2>
+                <p>Mint (M) Absolutely perfect in every way. Never been played and usually sealed.</p>
+                <p>Excellent (E) The vinyl has only been played a few times. There are no marks on the vinyl and the whole package is complete. Small signs of use.</p>
+                <p>Very Good Plus (VG+) A few further faults are acceptable, but nothing that compromises the record visually or audibly. Light inaudible marks or background crackle.</p>
+                <p>Very Good (VG) Visually and audibly used. Light pops and clicks, an edge split, light visible scratches. You can still enjoy listening and the record looks good.</p>
+                <p>Good (G) Good mean bad condition. Usually only highly sought after records would sell in this condition.</p>
+                <p>Fair (F) Expect major noise issues, skipping or repeating. The record itself is cracked, badly warped and has deep scratches.</p>
+        </div>
     )
 }
 
 export default VinylGrade
+
+
+
+
+
+
+
+
+
+
